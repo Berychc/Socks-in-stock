fce57c16ad4381bdb7fc7599e04fbe406f69e463
Berychc <treewincsgo@gmail.com>
2024-12-18 17:42:56 +0300

    Start project

---
+++ src/main/java/com/example/socksinstock/SocksinstockApplication.java
@@ -0,0 +1,13 @@
+package com.example.socksinstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksinstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksinstockApplication.class, args);
+	}
+
+}
